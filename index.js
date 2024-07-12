a115aaa8c9cf38684123ca944640931925e51986
Sakib-05 <sakibimamul03@gmail.com>
2024-07-13 00:25:32 +0100

    created Top Bar with Date and Time  📆 ⏰

---
+++ index.js
@@ -0,0 +1,13 @@
+let time = document.getElementById('current-time');
+let today = document.getElementById('current-date');
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+today.innerHTML = `${day}/${month}/${year}`;
+
+setInterval(() => {
+    let date = new Date();
+    time.innerHTML = date.toLocaleTimeString();
+
+}, 1000);
