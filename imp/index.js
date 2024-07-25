1ccae3168b0a3595f3e470ce8337658a4a2ac6be
Sakib-05 <sakibimamul03@gmail.com>
2024-07-25 20:53:33 +0100

    CV download button and finished projects page

---
+++ imp/index.js
@@ -0,0 +1,16 @@
+// let name;
+// let age;
+// let picture; 
+// document.getElementById('button').onclick = function() {
+//     name = document.getElementById('name').textContent;
+//     age = document.getElementById('age').textContent;
+//     text = document.getElementById('text').textContent;
+//     picture = document.getElementById('picture').textContent;
+//     document.getElementById('new').textContent = "name: " + name + " age: " + age + " text: " + text;
+//     document.getElementById('img').src = picture;
+
+// }
+
+// console.log(name);  
+// console.log(age);
+// console.log(text);
